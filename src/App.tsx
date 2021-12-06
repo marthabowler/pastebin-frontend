6bbf18a66c3f007c9dcaed34dc3f5e0cc275221b
Martha Bowler <mbowler133@gmail.com>
2021-12-06 17:55:31 +0000

    Fetch data from local backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import { greet } from "./utils/greet";
+import { useEffect, useState } from "react";
 
-function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+interface pasteBin {
+  id: number;
+  input: string;
+  title?: string;
+  creation_date: string;
 }
 
+function App(): JSX.Element {
+  const [pastebinState, setPastebinState] = useState<pasteBin[]>([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const resp = await fetch("http://localhost:4000/");
+      const jsonBody = await resp.json();
+      setPastebinState(jsonBody);
+    };
+    loadData();
+  }, [pastebinState]);
+
+  return <>{pastebinState.map((item) => item.input)}</>;
+}
 export default App;
